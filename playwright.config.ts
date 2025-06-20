0e70c2c42a4e83634de431ae756eeef4ecdfded0
ABOBA <ABOBA@SMART.ru>
2025-06-20 18:19:53 +0800

    new

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,37 +1,31 @@
 import { defineConfig } from '@playwright/test';
 
-const prNumber = process.env.PR_NUMBER;
-const isCI = Boolean(process.env.CI);
+const pr = process.env.PR_NUMBER;
 const repo = process.env.GITHUB_REPOSITORY;
+const isCI = Boolean(process.env.CI);
+
+if (!pr && isCI) throw new Error('Не передан PR_NUMBER');
+if (!repo && isCI) throw new Error('Не передан GITHUB_REPOSITORY');
+
 const [owner, repoName] = repo?.split('/') || [];
-
-if (isCI && (!prNumber || !owner || !repoName)) {
-  throw new Error('В CI нужно задать PR_NUMBER и GITHUB_REPOSITORY');
-}
-
 const baseURL = isCI
-  ? `https://${owner}.github.io/${repoName}/pr-${prNumber}/`
+  ? `https://${owner}.github.io/${repoName}/pr-${pr}/`
   : 'http://localhost:8080/';
 
 export default defineConfig({
   testDir: './tests',
   use: {
+    baseURL,
     screenshot: 'only-on-failure',
   },
   projects: [
     {
       name: 'desktop',
-      use: {
-        viewport: { width: 1440, height: 900 },
-        baseURL,
-      },
+      use: { viewport: { width: 1440, height: 900 } },
     },
     {
       name: 'mobile',
-      use: {
-        viewport: { width: 475, height: 800 },
-        baseURL,
-      },
+      use: { viewport: { width: 475, height: 800 } },
     },
   ],
 });
--- tests/visual.test.ts
+++ tests/visual.test.ts
@@ -4,17 +4,8 @@ const taskId = process.env.TASK_ID;
 if (!taskId) throw new Error('Не указана переменная окружения TASK_ID');
 
 test(`${taskId}`, async ({ page }) => {
-  const response = await page.goto('/');
-  console.log('Resolved page.goto URL:', response?.url());
-  console.log('Status:', response?.status());
-
-  if (!response || !response.ok() || response.status() === 404) {
-    throw new Error(`Страница не загрузилась корректно: ${response?.status()}`);
-  }
-
-  await page.waitForSelector('.container', { timeout: 10000 });
-
   const fileName = `${taskId}.png`;
+  await page.goto('/');
   await expect(page).toHaveScreenshot(fileName, {
     fullPage: true,
     animations: 'disabled',
