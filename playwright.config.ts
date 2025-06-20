92d1af67e46e96d8b17d46877b1a393f4c70c469
ABOBA <ABOBA@SMART.ru>
2025-06-20 17:43:40 +0800

    fix url

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,30 +2,31 @@ import { defineConfig } from '@playwright/test';
 
 const prNumber = process.env.PR_NUMBER;
 const isCI = Boolean(process.env.CI);
-
-if (!prNumber && isCI) {
-  throw new Error('В CI среде необходимо передать PR_NUMBER как переменную окружения');
-}
-
 const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+
+const baseURL = isCI
+  ? `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/${repoName}/pr-${prNumber}`
+  : 'http://localhost:8080';
 
 export default defineConfig({
   testDir: './tests',
   use: {
-    baseURL: isCI
-      ? `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/${repoName}/pr-${prNumber}`
-      : 'http://localhost:8080',
     screenshot: 'only-on-failure',
   },
-  // webServer убираем полностью
   projects: [
     {
       name: 'desktop',
-      use: { viewport: { width: 1440, height: 900 } },
+      use: {
+        viewport: { width: 1440, height: 900 },
+        baseURL,
+      },
     },
     {
       name: 'mobile',
-      use: { viewport: { width: 475, height: 800 } },
+      use: {
+        viewport: { width: 475, height: 800 },
+        baseURL,
+      },
     },
   ],
 });
--- tests/visual.test.ts
+++ tests/visual.test.ts
@@ -4,11 +4,7 @@ const taskId = process.env.TASK_ID;
 if (!taskId) throw new Error('Не указана переменная окружения TASK_ID');
 
 test(`${taskId}`, async ({ page }, testInfo) => {
-  console.log('Resolved URL:', page.url());
-  console.log('Environment:');
-  console.log('PR_NUMBER:', process.env.PR_NUMBER);
-  console.log('BASE_URL:', testInfo.project.use?.baseURL);
-
+  console.log('Resolved page.goto URL:', new URL('/', testInfo.project.use?.baseURL).toString());
   await page.goto('/');
   const fileName = `${taskId}.png`;
 
